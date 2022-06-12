bec48d5a55defd81373f0e778eecb46b376bbd43
Sh0r0 <107363275+Sh0r0@users.noreply.github.com>
2022-06-12 19:58:03 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_button_pressed_a():
+    basic.show_string("The temperature Is Above Zero")
+basic.show_string("The temperature Is Above Zero")
+basic.show_string("The temperature Is Zero")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+basic.show_string("Shoroug")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,11 @@
+    IfInput.temperature(<0)
+    basic.showString("The temperature Is Above Zero")
+        Ifinput.temperature()<0
+basic.showString("The temperature Is Zero")
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    ElseElse:
+    basic.showString("The temperature Is Above Zero")
+basic.showString("Shoroug")
+basic.forever(function on_forever() {
+    
+})
